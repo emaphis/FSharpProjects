7e8ed707eabbb8a26fd1c793f0870e6f72735cf8
emaphis <emaphis85@gmail.com>
2022-11-12 23:18:22 -0500

    Exercises 3-5

--- functional/chap02/Exercise03.fsx
+++ functional/chap02/Exercise03.fsx
@@ -2,9 +2,10 @@
 //     isIthChar(str, i, ch) is true if and only if ch is the i’th character in the string str (numbering
 //     starting at zero)
 
-let isIthChar (str: string) ith char =
-    str.[ith] = char
+let isIthChar (str: string, i, ch) = str.[i] = ch 
 
-true = isIthChar "a" 0 'a'
-true = isIthChar "abc" 0 'a'
-false = isIthChar "abc" 1 'a'
+true = isIthChar ("a", 0, 'a')
+false = isIthChar ("b", 0, 'a')
+true = isIthChar ("abc", 0, 'a')
+true = isIthChar ("abc", 1, 'b')
+false = isIthChar ("abc", 1, 'a')
