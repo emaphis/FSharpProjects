e616a7d16a0b36775a68c54f080d2a8ba7e02ee0
emaphis <emaphis85@gmail.com>
2022-11-16 22:11:13 -0500

    Update .gitignore

---
+++ functional/chap03/Script.fsx
@@ -0,0 +1,12 @@
+// Chapter 3 - Tuples, records and tagged values
+
+// 3.1 Tuples
+
+(10, true)
+(("abc", 1), -3)
+
+// Tuple patterns
+let (x, n) = (3, 2)
+
+// patterns with constants
+let (x1, 0) = ((3, "a"), 0)
