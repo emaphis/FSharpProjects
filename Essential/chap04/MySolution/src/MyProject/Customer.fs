ddcebaaa15d564335a422da3fb920f64645b2152
emaphis <emaphis85@gmail.com>
2022-12-03 18:52:28 -0500

    Project organization and unit tests.

---
+++ Essential/chap04/MySolution/src/MyProject/Customer.fs
@@ -0,0 +1,33 @@
+﻿module MyProject.Customer
+
+
+type Customer = {
+    Id : int
+    IsVip : bool
+    Credit : decimal
+}
+
+
+
+// Customer -> (Customer * decimal)
+let getPurchases customer =
+    let purchases = if customer.Id % 2 = 0 then 120M else 80M
+    (customer, purchases)
+
+// (Customer * decimal) -> Customer
+let tryPromoteToVip purchases =
+    let (customer, amount) = purchases
+    if amount > 100M then { customer with IsVip = true }
+    else customer
+
+// Customer -> Customer
+let increaseCreditIfVip customer =
+    let increase = if customer.IsVip then 100M else 50M
+    { customer with Credit = customer.Credit + increase }
+
+// Customer -> Customer
+let upgradeCustomer customer = 
+    customer
+    |> getPurchases
+    |> tryPromoteToVip
+    |> increaseCreditIfVip
