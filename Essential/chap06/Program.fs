825c3190a9d8e68a42c86d6d0f4cb1a03792966c
emaphis <emaphis85@gmail.com>
2022-12-04 14:14:16 -0500

    Reading data from files

--- Essential/chap06/Program.fs
+++ Essential/chap06/Program.fs
@@ -1,2 +1,65 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿
+open System.IO
+
+
+type Customer = {
+    CustomerId : string
+    Email : string
+    IsElegible : string
+    IsRegistered : string
+    DateRegistered : string
+    Discount : string
+}
+
+type DataReader = string -> Result<string seq,exn>
+
+
+let readFile : DataReader =
+    fun path ->
+        try 
+            File.ReadLines(path)
+            |> Ok
+        with
+        | ex -> Error ex
+
+
+let parseLine (line: string) : Customer option =
+    match line.Split('|') with
+    | [| customerId; email; eligible; registered; dateRegistered; discount |] ->
+        Some {
+            CustomerId = customerId
+            Email = email
+            IsElegible = eligible
+            IsRegistered = registered
+            DateRegistered = dateRegistered
+            Discount = discount
+        }
+    | _ -> None
+
+
+let parse (data: string seq) =
+    data
+    |> Seq.skip 1  // ignore the header row
+    |> Seq.map parseLine
+    |> Seq.choose id   // ignore Nane and unwratpe Some
+
+
+let output data =
+    data
+    |> Seq.iter (fun x -> printfn "%A" x)
+
+
+let import (dataReader: DataReader) path =
+    match path |> dataReader with
+    | Ok data -> data |> parse |> output
+    | Error ex -> printfn "Error: %A" ex.Message
+
+
+let importWithFileReader = import readFile
+
+
+[<EntryPoint>]
+let main argv =
+    Path.Combine(__SOURCE_DIRECTORY__, "resources", "customers.csv")
+    |> importWithFileReader
+    0
