a7192ba9a119b09ef2fb1cad7156f5b4832f2c0d
emaphis <emaphis85@gmail.com>
2024-05-09 23:37:38 -0400

    Lesson 11 - Result Computational Expressions

---
+++ Essentials/Lesson11/OptionComputationExpressionDemo/ResultsDemo/Program.fs
@@ -0,0 +1,12 @@
+﻿open ComputationExpression.ResultDemo
+
+let cust1 = { Id = 1; IsVip = false; Credit = 0m }
+let cust2 = { Id = 2; IsVip = false; Credit = 200m }
+
+cust1
+|> upgradeCustomerCU
+|> printfn "Cust1: %A"
+
+cust2
+|> upgradeCustomerCU
+|> printfn "Cust2: %A"
