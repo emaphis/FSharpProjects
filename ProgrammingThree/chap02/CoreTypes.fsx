65103be4ca84a45576727bb7460d13bc45bd599c
emaphis <emaphis85@gmail.com>
2025-10-31 23:13:48 -0400

    Lists.

--- ProgrammingThree/chap02/CoreTypes.fsx
+++ ProgrammingThree/chap02/CoreTypes.fsx
@@ -98,3 +98,179 @@ let sum1 = tupleAdd(3, 4)
 
 
 
+// Lists
+
+// Defining
+let vowels = [ 'a'; 'e'; 'i'; 'o'; 'u' ]
+let emptyList = []
+
+
+// primative operators
+
+// cons
+let sometimes = 'y' :: vowels 
+//val sometimes: char list = ['y'; 'a'; 'e'; 'i'; 'o'; 'u']
+
+// applend '@'
+let odds = [ 1; 3; 5; 7; 9 ]
+let evens = [ 2; 4; 6; 8; 10 ]
+
+let ints = odds @ evens
+//val ints: int list = [1; 3; 5; 7; 9; 2; 4; 6; 8; 10]
+
+
+// ranges
+
+let x2 = [ 1 .. 10 ]
+//val x2: int list = [1; 2; 3; 4; 5; 6; 7; 8; 9; 10]
+
+let tens = [ 0 .. 10 .. 50 ]
+//val tens: int list = [0; 10; 20; 30; 40; 50]
+
+let countdown = [ 5L .. -1L .. 0L ]
+//val countdown: int64 list = [5L; 4L; 3L; 2L; 1L; 0L]
+
+
+// List comprehensions
+
+// Simple list comprehensin
+let numbersNear x =
+    [
+        yield x - 1
+        yield x
+        yield x + 1
+    ]
+//val numbersNear: x: int -> int list
+
+let near3 = numbersNear 3
+//val near3: int list = [2; 3; 4]
+
+// More complex example
+let x4 =
+    [  let negate x = -x
+       for i in 1 .. 10 do
+            if i % 2 = 0 then
+                yield negate i
+            else 
+                yield i ]
+//val x4: int list = [1; -2; 3; -4; 5; -6; 7; -8; 9; -10]
+
+//  using for loops within list comprehensions, you can simplify the code by
+//  using -> insteadof do
+
+/// Generates the first ten multiples of a number
+let multiplesOfa x = [ for i in 1 .. 10 do yield x * i ]
+
+/// Generates the first ten multiples of a number simplified
+let multiplesOfb x = [ for i in 1 .. 10 -> x * i ]
+
+let multiplesOf = multiplesOfb 3
+//val multiplesOf: int list = [3; 6; 9; 12; 15; 18; 21; 24; 27; 30]
+
+
+// Example 2-3. Using list comprehensions to compute primes
+
+/// List comprehension for prime numbers
+let primesUnder max =
+    [
+        for n in 1 .. max do
+            let factorOfN =
+                [
+                    for i in 1 .. n do
+                        if n % i = 0 then
+                            yield i
+                ]
+
+            // n is prime if its only factors are 1 and n
+            if List.length factorOfN = 2 then
+                yield n
+    ]
+//val primesUnder: max: int -> int list
+
+let primesUnder50 = primesUnder 50
+//val primesUnder50: int list =
+//   [2; 3; 5; 7; 11; 13; 17; 19; 23; 29; 31; 37; 41; 43; 47]
+
+
+//  List module functions
+
+// Using List.partition
+let isMultipleOf5 x = (x % 5 = 0)
+
+let list5 = [ 1 .. 15 ]
+
+let multOf5, nonMultOf5 =
+    List.partition isMultipleOf5 list5
+
+//val nonMultOf5: int list = [1; 2; 3; 4; 6; 7; 8; 9; 11; 12; 13; 14]
+//val multOf5: int list = [5; 10; 15]
+
+
+// Aggregate Operators
+
+List.map // each new element of the list is the result of applying the passed function
+//val it: (('a -> 'b) -> 'a list -> 'b list)
+
+//  Example 2-4. Using List.map to square numbers in a list
+
+let square' x = x * x
+
+let squares = List.map square' [ 1 .. 10 ]
+//val squares: int list = [1; 4; 9; 16; 25; 36; 49; 64; 81; 100]
+
+
+List.reduce
+//val it: (('a -> 'a -> 'a) -> 'a list -> 'a)
+
+// Example 2-5. Comma separating a list of strings using List.reduce
+let insertCommas (acc: string) item = acc + ", " + item
+
+let text1 = List.reduce insertCommas  ["Jack"; "Jill"; "Jim"; "Joe"; "Jane"]
+//al text1: string = "Jack, Jill, Jim, Joe, Jane"
+
+
+List.fold
+//val it: (('a -> 'b -> 'a) -> 'a -> 'b list -> 'a)
+
+let addAccToListItem acc i = acc + i
+
+let sum = List.fold addAccToListItem 0 [1; 2; 3]
+//val sum: int = 6
+
+// The accumulator doesn't have to be the same type as the list.
+
+/// Count the numbers of vowels in a string
+let countVowels (str: string) =
+    let charList = List.ofSeq str
+
+    let accFunct (As, Es, Is, Os, Us) letter =
+        if   letter = 'a' then (As + 1, Es, Is, Os, Us)
+        elif letter = 'e' then (As, Es + 1, Is, Os, Us)
+        elif letter = 'i' then (As, Es, Is + 1, Os, Us)
+        elif letter = 'o' then (As, Es, Is, Os + 1, Us)
+        elif letter = 'u' then (As, Es, Is, Os, Us + 1)
+        else                   (As, Es, Is, Os, Us)
+
+    List.fold accFunct (0, 0, 0, 0, 0) charList
+
+let counts =  countVowels "The quick brown fox jumps over the lazy dog"
+
+
+// Folding right-to-left
+
+List.reduceBack
+//val it: (('a -> 'a -> 'a) -> 'a list -> 'a)
+
+List.foldBack
+//val it: (('a -> 'b -> 'b) -> 'a list -> 'b -> 'b)
+
+
+List.iter
+//val it: (('a -> unit) -> 'a list -> unit)
+
+// Example 2-7. Using List.iter to print numbers in a list
+
+
+let printNumber x = printfn "Printing %d" x
+
+do List.iter printNumber [1..5]
