ab4012e5c7e08f5985c44835f901ae62bb6e207b
Ed <emaphis85@gmail.com>
2026-06-12 22:18:11 -0400

    Flow control and arrays.

---
+++ Tutorial/01Basics/03ImperativeProgramming/Arrays.fsx
@@ -0,0 +1,74 @@
+﻿// Imperative Programming
+// Arrays
+
+// Creating Arrays
+
+// Array literals
+
+let ar1 = [| 1; 2; 3; 4; 5|]
+
+// Array comprehensions
+
+let ar2 = [| 1 .. 10 |]
+// val ar2: int array = [|1; 2; 3; 4; 5; 6; 7; 8; 9; 10|]
+
+let ar3 = [| 1 .. 3 .. 10 |]
+// al ar3: int array = [|1; 4; 7; 10|]
+
+let ar4 =
+    [| for a in 1 .. 5 do
+        yield (a, a*a, a*a*a)|]
+// val ar4: (int * int * int) array =
+//   [|(1, 1, 1); (2, 4, 8); (3, 9, 27); (4, 16, 64); (5, 25, 125)|]
+
+
+// System.Array Methods
+
+// val zeroCreate : int arraySize -> 'T []
+// Creates an array with arraySize elements. Each element in the array holds the default value for the particular data type (0 for numbers, false for bools, null for reference types).
+
+let (ar5: int array) = Array.zeroCreate 5
+// val ar5: int array = [|0; 0; 0; 0; 0|]
+ar5
+//val it: int array = [|0; 0; 0; 0; 0|
+
+
+// val create : int -> 'T value -> 'T []
+// Creates an array with arraySize elements. Initializes each element in the array with value
+let ar6 = Array.create 5 "Juliet"
+//al ar6: string array = [|"Juliet"; "Juliet"; "Juliet"; "Juliet"; "Juliet"|]
+
+
+// val init : int arraySize -> (int index -> 'T) initializer -> 'T []
+// Creates an array with arraySize elements. Initializes each element in the array with the initializer function.
+
+let ar7 = Array.init 5 (fun idx -> sprintf $"idx = {idx}")
+// al ar7: string array =
+//    |"idx = 0"; "idx = 1"; "idx = 2"; "idx = 3"; "idx = 4"|]
+
+
+// Working With Arrays
+
+let names = [| "Juliet"; "Monique"; "Rachelle"; "Tara"; "Sophia" |]
+
+let str1 = names[2]
+//val str1: string = "Rachelle"
+
+let str2 = names[0]
+//al str2: string = "Juliet"
+
+let len1 = names.Length
+//val len1: int = 5
+
+for i = 0 to names.Length - 1 do
+    printfn  $"{names[i]}"
+
+
+// arrays are mutable.
+
+names
+
+names[4] <- "Kristen"
+
+names
+
---
+++ Tutorial/01Basics/03ImperativeProgramming/ControlFlow.fsx
@@ -0,0 +1,142 @@
+﻿// Imperative Programming
+// Control Flow
+
+(*
+ Imperative programming can be useful in the following situations:
+
+ * Interacting with many objects in the .NET Framework, most of which
+   are inherently imperative.
+ * Interacting with components that depend heavily on side-effects,
+   such as GUIs, I/O, and sockets.
+ * Scripting and prototyping snippets of code.
+ * Initializing complex data structures.
+ * Optimizing blocks of code where an imperative version of an
+   algorithm is more efficient than the functional version.
+*)
+
+// if/then Decisions
+
+// scopes
+
+let printMessage condition =
+    if condition then
+        printfn "condition = true: inside the 'if'"
+    printfn "outside the 'if' block"
+
+let main1() =
+    printMessage true
+    printfn "--------"
+    printMessage false
+    //Console.ReadKey(true) |> ignore
+ 
+main1()
+
+
+// a demonstration of short-circuiting in F#
+
+let alwaysTrue() =
+    printfn "Always true"
+    true
+    
+let alwaysFalse() =
+    printfn "Always false"
+    false
+
+let main2() =
+    let testCases = 
+        ["alwaysTrue && alwaysFalse", fun() -> alwaysTrue() && alwaysFalse();
+         "alwaysFalse && alwaysTrue", fun() -> alwaysFalse() && alwaysTrue();
+         "alwaysTrue || alwaysFalse", fun() -> alwaysTrue() || alwaysFalse();
+         "alwaysFalse || alwaysTrue", fun() -> alwaysFalse() || alwaysTrue();]
+    
+    testCases |> List.iter (fun (msg, res) ->
+        printfn "%s: %b" msg (res())
+        printfn "-------")
+
+
+main2()
+
+
+// for Loops Over Ranges
+
+(*
+for var = start-expr to end-expr do
+... // loop body
+*)
+
+
+let main3() =
+    for i = 1 to 10 do
+        printfn $"i: {i}"
+
+
+main3()
+
+
+// This code takes input from the user to compute an average:
+
+open System
+
+let main4() =
+    Console.WriteLine("This program averages numbers input by the user.")
+    Console.Write("How many numbers do you want to add? ")
+
+    let mutable sum = 0
+    let numbersToAdd =  Console.ReadLine() |> int
+
+    for i = 1 to numbersToAdd do
+        Console.Write("Input #{0}: ", i)
+        let input = Console.ReadLine() |> int
+        sum <- sum + input
+
+    let average = sum / numbersToAdd
+    Console.WriteLine("Average: {0}", average)
+
+
+main4()
+
+
+// for Loops Over Collections and Sequences
+
+(*
+for pattern in expr do
+... // loop body
+*)
+
+// print out a shopping list
+
+let shoppingList =
+    ["Tofu", 2, 1.99;
+    "Seitan", 2, 3.99;
+    "Tempeh", 3, 2.69;
+    "Rice milk", 1, 2.95;]
+
+for (food, quantity, price) in shoppingList do
+    printfn $"foord: {food}, quantity: {quantity}, price: %g{price}"
+
+
+// while Loops
+(*
+while expr do
+... // loop body
+*)
+
+open System
+
+let main5() =
+    let password = "monkey"
+    let mutable guess = String.Empty
+    let mutable attempts = 0
+
+    while password <> guess && attempts < 3 do
+        Console.Write("What is the password? ")
+        attempts <- attempts + 1
+        guess <- Console.ReadLine()
+
+    if password = guess then
+        Console.WriteLine("You got the password right!")
+    else
+        Console.WriteLine("You didn't get the passord")
+
+
+main5()
